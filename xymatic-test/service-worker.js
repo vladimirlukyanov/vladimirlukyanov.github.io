14ad65918e946ef7429112ab9cbbe380184c5e09
Vladimir Lukyanov <vladimir@liikyanov.com>
2020-09-17 09:46:39 +0300

    v1.0.22 – relative path fixed

--- xymatic-test/service-worker.js
+++ xymatic-test/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8d00b80e7dac573114b8514b1926efd.js"
+  "./precache-manifest.4d4f27cd7474268d0e63b0d9c5a40fd4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
